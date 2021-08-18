a19c7494a7c95a5f3881f45dc69dd1515b9e372a
root <oguma292@yahoo.co.jp>
2021-08-18 12:13:31 +0000

    rayout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static_pages#help'
   resources :tweets
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
