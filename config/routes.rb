e3a6fc84188672956a9b059acfc835dfeecfb88d
oguma0923 <oguma292@yahoo.co.jp>
2021-08-20 07:28:29 +0000

    basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  resources :tweets
-  resources :users
   get 'signup' => 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tweets
+  resources :users
 end
